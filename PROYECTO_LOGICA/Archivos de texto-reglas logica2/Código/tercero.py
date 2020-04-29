3394de551e1ae4ed1510b8406551c70a59f9ee04
EstefaniaLaverde <57509269+EstefaniaLaverde@users.noreply.github.com>
2020-04-28 19:38:00 -0500

    Funciones de las reglas
    issue no: AD010

---
+++ PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/tercero.py
@@ -0,0 +1,33 @@
+def REGLA3():
+    archivo=open("Regla_3_aux.txt", "w")
+    Posiciones=["Arriba","Medio","Abajo"]
+    Palos=["PA","PB","PC"]
+    Fichas=["F1","F2","F3"]
+    Turnos=["1","2","3","4","5","6","7","8"]
+
+
+    for T in Turnos:
+        for PX in Palos:
+            W="("+PX+",F1,Abajo,"+T+")<>¬("+PX+",F2,Medio,"+T+")Y"
+            archivo.write(W)
+            archivo.write("\n")
+
+    archivo.close()
+
+    archivo2=open("Regla_3_aux.txt", "r")
+    x=archivo2.readlines()
+
+    regla3=[]
+    for l in range(len(x)):
+
+        if l==len(x)-1:
+            new_line="("+"PC"+",F1,Abajo,"+"8"+")<>¬("+"PC"+",F2,Medio,"+"8"+")"
+            regla3.append(new_line)
+        else:
+            regla3.append(x[l])
+    archivo2.close()
+    archivo_final=open("Regla_3.txt","w")
+    for linea in regla3:
+        archivo_final.write(linea)
+
+    archivo_final.close()
