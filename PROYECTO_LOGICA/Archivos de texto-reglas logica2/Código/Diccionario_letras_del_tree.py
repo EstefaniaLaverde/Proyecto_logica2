4e070f6918bea72615116c6ebc02ad28725a75b5
EstefaniaLaverde <57509269+EstefaniaLaverde@users.noreply.github.com>
2020-04-29 21:22:35 -0500

    Update file Diccionario_letras_del_tree.py
    issue no: UD042

--- PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/Diccionario_letras_del_tree.py
+++ PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/Diccionario_letras_del_tree.py
@@ -1,45 +1,53 @@
 """=====FUNCIONES PARA PASAR LAS REGLAS A ARBOLES====="""
 
 
-Posiciones=["Arriba","Medio","Abajo"]
-Palos=["PA","PB","PC"]
-Fichas=["1","2","3"]
-Turnos1=["1","2","3","4","5","6","7","8"]
-Posiciones2=["Medio","Abajo"]
-Posiciones3=["Abajo"]
+"""FUNCION QUE LA LISTA CON LAS LETRAS PROPOSICIONALES LARGAS"""
+
+def crear_lista_letras_largas(): #Las letras "largas" tienen la forma (PA,1,Arriba,1)
+
+    Posiciones=["Arriba","Medio","Abajo"]
+    Palos=["PA","PB","PC"]
+    Fichas=["1","2","3"]
+    Turnos1=["1","2","3","4","5","6","7","8"]
+    Posiciones2=["Medio","Abajo"]
+    Posiciones3=["Abajo"]
 
 
 
-#Codificación de la ficha 1
-ficha_1=[]
-for turno in Turnos1:
-    for pos in Posiciones:
-        for palo in Palos:
-            ficha_1.append("("+palo+","+"1"+","+pos+","+turno+")")
+    #Codificación de la ficha 1
+    ficha_1=[]
+    for turno in Turnos1:
+        for pos in Posiciones:
+            for palo in Palos:
+                ficha_1.append("("+palo+","+"1"+","+pos+","+turno+")")
 
-#Codificación de la ficha 2
-ficha_2=[]
-for turno in Turnos1:
-    for pos in Posiciones2:
-        for palo in Palos:
-            ficha_2.append("("+palo+","+"2"+","+pos+","+turno+")")
+    #Codificación de la ficha 2
+    ficha_2=[]
+    for turno in Turnos1:
+        for pos in Posiciones2:
+            for palo in Palos:
+                ficha_2.append("("+palo+","+"2"+","+pos+","+turno+")")
 
-#Codificación de la ficha 3
-ficha_3=[]
-for turno in Turnos1:
-    for pos in Posiciones3:
-        for palo in Palos:
-            ficha_3.append("("+palo+","+"3"+","+pos+","+turno+")")
+    #Codificación de la ficha 3
+    ficha_3=[]
+    for turno in Turnos1:
+        for pos in Posiciones3:
+            for palo in Palos:
+                ficha_3.append("("+palo+","+"3"+","+pos+","+turno+")")
 
-lista_letras_largas=[] #ESTA LISTA VA A ACTUAR COMO lista_prop EN crear_diccionario
-for p in ficha_1:
-    lista_letras_largas.append(p)
-for p in ficha_2:
-    lista_letras_largas.append(p)
-for p in ficha_3:
-    lista_letras_largas.append(p)
+    lista_letras_largas=[] #ESTA LISTA VA A ACTUAR COMO lista_prop EN crear_diccionario
+    for p in ficha_1:
+        lista_letras_largas.append(p)
+    for p in ficha_2:
+        lista_letras_largas.append(p)
+    for p in ficha_3:
+        lista_letras_largas.append(p)
 
+    return lista_letras_largas
 
+lista_letras_largas=crear_lista_letras_largas()
+
+#Esta lista letras contiene la letra correspondiente a cada letra larga, que se unen en un diccionario
 letras=['J', 'ħ', 'ί', 'ζ', 'É', 'j', 'Ắ', 'Ớ', 'Ẁ', 'Ī', 'ř', 'Ķ', 'Ễ', 'ớ', 'э', 'ⁿ', 'Ā', 'ы', 'c', 'ü', 'Ô', 'ł', 'K', 'đ', 'Ó', 'ь', 'Ŀ', 'Ề', 'Ð', 'ó', 'Ч', 'q', 'Ľ', 'Ј', 'ć', 'ý', 'ų', 'С', 'Р', 'o', 'Ę', 'Ҝ', 'у', 'В', 'H', 'Θ', 'О', 'ĉ', 'ν', 'Ң', 'А', 'z', 'ự', 'η', 'Ẵ', 'ẃ', 'ŷ', 'х', 'œ', 'Ä', '฿', 'Ằ', 'Ŏ', 'Ď', 'ј', 'ē', 'Z', 'ử', 'Ο', 'Ў', 'Ό', 'М', 'Ε', 'ŏ', 'Є', 'н', 'Ђ', 'ŵ', 'қ', 'Ŝ', 'ņ', 'д', 'п', 'Ö', 'Қ', 'Б', 'Ν', 'Ể', 'т', 'Ĺ', 'с', 'Ů', 'Џ', 'Х', 'h', 'Â', 'ї', 'Ò', '£', 'μ', 'Ă', 'Ặ', 'Ѓ', 'Ζ', 'о', 'ã', 'ĩ', 'ằ', 'ọ', 'Ŭ', 'ς', 'Ù', 'ð', 'Ъ', 'ö', 'Ҳ', 'Ç', 'n', 'ο', 'ů', 'è', 'Į', 'Ẃ', 'Μ', 'Á', 'Č', 'ŋ', 'υ', 'β', 'ī', 'Ş', 't', 'ª', 'ž', 'κ', 'ш', 'G', 'Í', 'Ї', '∑', 'ĸ', 'Ќ', 'ђ', 'Ģ']
 
 
@@ -62,9 +70,9 @@ Diccionario_final=crear_diccionario(lista_letras_largas, letras) #Aquí guardo e
 #print(Diccionario_final)
 
 
-"""FUNCIÓNES PARA TRANSFORMAR REGLAS A ARBOLES"""
+"""FUNCIÓN PARA TRANSFORMAR UNA REGLA CON LAS LETRAS CORTAS"""
 
-def letras_largas_a_char(regla, diccionario_traducci):
+def letras_largas_a_cortas(regla, diccionario_traducci):
     #input: un string con toda la regla y el diccionario para cambiar las letras
     #output: la misma regla pero con las nuevas letras proposicionales de un caracter.
 
@@ -106,7 +114,7 @@ def letras_largas_a_char(regla, diccionario_traducci):
 #"""Prueba de la función"""
 #regla_test="((PA, 1, Arriba, 6)Y((PA ,1 ,Medio ,2)Y(PB,1,Abajo,7)))"
 #print("Regla inicial: ",regla_test)
-#nueva_regla=letras_largas_a_char(regla_test,Diccionario_final)
+#nueva_regla=letras_largas_a_cortas(regla_test,Diccionario_final)
 
 #print("Regla final: ",nueva_regla)
 #"""FUNCIONA!!"""
@@ -142,6 +150,6 @@ def preparar_regla_para_cambiar_letras(archivo_de_texto):
 
 #"""Prueba de la función""""
 #regla_lista=preparar_regla_para_cambiar_letras("Regla_5_test.txt")
-#regla_nuevos_char=letras_largas_a_char(regla_lista, Diccionario_final)
+#regla_nuevos_char=letras_largas_a_cortas(regla_lista, Diccionario_final)
 
 #print(regla_nuevos_char)
