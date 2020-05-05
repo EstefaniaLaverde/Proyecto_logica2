854d5820931b1b246478f5947f5a16f3c078e342
EstefaniaLaverde <57509269+EstefaniaLaverde@users.noreply.github.com>
2020-05-05 14:16:53 -0500

    Modificaciones a la regla 3
    issue no: UD052

--- PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/Funciones generadoras de reglas/tercero.py
+++ PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/Funciones generadoras de reglas/tercero.py
@@ -1,33 +1,26 @@
 def REGLA3():
-    archivo=open("Regla_3_aux.txt", "w")
+    archivo=open("Regla_3.txt", "w")
     Posiciones=["Arriba","Medio","Abajo"]
     Palos=["PA","PB","PC"]
-    Fichas=["F1","F2","F3"]
+    Fichas=["1","2","3"]
     Turnos=["1","2","3","4","5","6","7","8"]
 
-
+    lista_aux=[]
     for T in Turnos:
         for PX in Palos:
-            W="("+PX+",F1,Abajo,"+T+")<>-("+PX+",F2,Medio,"+T+")Y"
-            archivo.write(W)
-            archivo.write("\n")
+            W="("+PX+",1,Abajo,"+T+")<>-("+PX+",2,Medio,"+T+")Y"
+            lista_aux.append(W)
 
-    archivo.close()
-
-    archivo2=open("Regla_3_aux.txt", "r")
-    x=archivo2.readlines()
-
-    regla3=[]
-    for l in range(len(x)):
-
-        if l==len(x)-1:
-            new_line="("+"PC"+",F1,Abajo,"+"8"+")<>-("+"PC"+",F2,Medio,"+"8"+")"
-            regla3.append(new_line)
+    Regla3="("
+    i=0
+    for i in range(len(lista_aux)):
+        if i==len(lista_aux)-1:
+            new_line="("+"PC"+",1,Abajo,"+"8"+")<>-("+"PC"+",2,Medio,"+"8"+")" #Regla sin Y final
+            Regla3+=new_line
         else:
-            regla3.append(x[l])
-    archivo2.close()
-    archivo_final=open("Regla_3.txt","w")
-    for linea in regla3:
-        archivo_final.write(linea)
+            Regla3+=lista_aux[i]
+        i+=1
 
-    archivo_final.close()
+    Regla3+=")" #final de la regla
+    archivo.write(Regla3)
+    archivo.close()
