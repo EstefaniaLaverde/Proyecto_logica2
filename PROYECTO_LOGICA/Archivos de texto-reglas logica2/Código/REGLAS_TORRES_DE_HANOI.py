7a1de4edc1de81042327325afd776732e7836e8f
EstefaniaLaverde <57509269+EstefaniaLaverde@users.noreply.github.com>
2020-05-08 22:48:00 -0500

    Udpadate Reglas 4 y 5: ya no crean archivos auxiliares
    issue no: UD059

--- PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/REGLAS_TORRES_DE_HANOI.py
+++ PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/REGLAS_TORRES_DE_HANOI.py
@@ -4,8 +4,12 @@
 import Codificacion as C
 
 #FALTA LA REGLA 1
+def REGLA1(): #Una situación por turno
+    #Implementar aquí la regla 1
+    pass
 
-def REGLA2():
+
+def REGLA2(): #No levitación
     Posiciones=["Arriba","Medio","Abajo"]
     Palos=["PA","PB","PC"]
     Fichas=["1","2","3"]
@@ -56,10 +60,11 @@ def REGLA2():
         archivo.write('\n')
 
     archivo.close()
-    return C.CODIFICAR("Regla_2.txt")
+    retorno=C.CODIFICAR("Regla_2.txt")
+    return retorno
 
 
-def REGLA3():
+def REGLA3(): #No mayor sobre menor
     archivo=open("Regla_3.txt", "w")
     Posiciones=["Arriba","Medio","Abajo"]
     Palos=["PA","PB","PC"]
@@ -85,330 +90,271 @@ def REGLA3():
     Regla3+=")" #final de la regla-> AQUÍ SE GUARDA SIN CODIFICAR
     archivo.write(Regla3)
     archivo.close()
-    return C.CODIFICAR("Regla_3.txt")
+    retorno = C.CODIFICAR("Regla_3.txt")
+    return retorno
 
 
-def REGLA4():
-    archivo=open("cuarto1.txt","w")
+def REGLA4(): #Un movimiento por turno
     Posiciones=["Arriba","Medio","Abajo"]
     Palos=["PA","PB","PC"]
     Fichas=["F1","F2","F3"]
     Turnos=["1","2","3","4","5","6","7","8"]
-    i=0
+
     #M1,N
-    while i<7:#para los turnos
-        parentesis="["
-        archivo.write(parentesis)
+    M1N=[]
+    i=0
+    while i<7:
         for PX in Palos:
             for P in Posiciones:
-                W="[("+PX+", 1, "+P+", "+Turnos[i]+")<>("+PX+", 1, "+P+", "+Turnos[i+1]+")]Y"
-                archivo.write(W)
-        archivo.write("\n")
+                W="[("+PX+", 1, "+P+", "+Turnos[i]+")<>("+PX+", 1, "+P+", "+Turnos[i+1]+")]"
+                M1N.append(W)
         i+=1
-    archivo.close()
-    amix=open("cuarto1.txt","r")
-    test=[]
-    lista_de_cuarto1=[]
-    lineas_de_cuarto1=amix.readlines()
-    for linea in lineas_de_cuarto1:
-        lista_de_cuarto1.append(linea)
-    for linea in lista_de_cuarto1:
-        nueva_linea=linea[0:-2]
-        nueva_linea+="]"
-        test.append(nueva_linea)
-    amix.close()
 
-    archivo_def=open("cuarto1def.txt","w")#en este archivo se encuentra M1,N definitivo    Esta todo unido con "y"
-    for linea in test:
-        archivo_def.write(linea)
-        archivo_def.write('\n')
-    archivo_def.close()
+    cont=0
+    prop=""
+    M1Ndef=[]
+    for pp in M1N:
+        if cont==0:
+            prop+="["+pp+"Y"
+            cont+=1
+        elif cont<8:
+            prop+=pp+"Y"
+            cont+=1
+        elif cont==8:
+            cont=0
+            prop+=pp+"]"
+            M1Ndef.append(prop)
+            prop=""
 
+    """for p in M1Ndef:
+        print(p)"""
 
+    #LISTO M1N
+    #==================================================================================================================
+    #M2N
 
-
-    archivo2=open("cuarto2.txt","w")
+    #Para este hay otras Posiciones
     Posiciones2=["Medio","Abajo"]
-    #M2,n
-    I=0
-    while I<7:
-        parentesis="["
-        archivo2.write(parentesis)
+
+    M2N=[]
+
+    i=0
+    while i<7:
         for PX in Palos:
             for P in Posiciones2:
-                W="[("+PX+", 2, "+P+", "+Turnos[I]+")<>("+PX+", 2, "+P+", "+Turnos[I+1]+")]Y"
-                archivo2.write(W)
-        archivo2.write("\n")
-        I+=1
-    archivo2.close()
-    amix2=open("cuarto2.txt","r")
-    test2=[]
-    lista_de_cuarto2=[]
-    lineas_de_cuarto2=amix2.readlines()
-    for linea in lineas_de_cuarto2:
-        lista_de_cuarto2.append(linea)
-    for linea in lista_de_cuarto2:
-        nueva_linea=linea[0:-2]
-        nueva_linea+="]"
-        test2.append(nueva_linea)
-    amix2.close()
-    archivo_def2=open("cuarto2def.txt","w")#en este archivo se encuentra M2,N definitivo    Esta todo unido con "y"
-    for linea in test2:
-        archivo_def2.write(linea)
-        archivo_def2.write('\n')
-    archivo_def2.close()
+                W="[("+PX+", 2, "+P+", "+Turnos[i]+")<>("+PX+", 2, "+P+", "+Turnos[i+1]+")]"
+                M2N.append(W)
+        i+=1
 
+    cont=0
+    prop=""
+    M2Ndef=[]
+    for pp in M2N:
+        if cont==0:
+            prop+="["+pp+"Y"
+            cont+=1
+        elif cont<5:
+            prop+=pp+"Y"
+            cont+=1
 
+        elif cont==5:
+            cont=0
+            prop+=pp+"]"
+            M2Ndef.append(prop)
+            prop=""
 
+    """for p in M2Ndef:
+        print(p)"""
 
-    archivo3=open("cuarto3.txt","w")
+    #Listo M2N
+    #==================================================================================================
+    #M3N
+
+    #Para este hay otras Posiciones
     Posiciones3=["Abajo"]
-    #M3,n
-    I=0
-    while I<7:
-        parentesis="["
-        archivo3.write(parentesis)
+    M3N=[]
+
+    i=0
+    while i<7:
         for PX in Palos:
             for P in Posiciones3:
-                W="[("+PX+", 3, "+P+", "+Turnos[I]+")<>("+PX+", 3, "+P+", "+Turnos[I+1]+")]Y"
-                archivo3.write(W)
-        archivo3.write("\n")
-        I+=1
-    archivo3.close()
-    amix3=open("cuarto3.txt","r")
-    test3=[]
-    lista_de_cuarto3=[]
-    lineas_de_cuarto3=amix3.readlines()
-    for linea in lineas_de_cuarto3:
-        lista_de_cuarto3.append(linea)
-    for linea in lista_de_cuarto3:
-        nueva_linea=linea[0:-2]
-        nueva_linea+="]"
-        test3.append(nueva_linea)
-    amix3.close()
-    archivo_def3=open("cuarto3def.txt","w")#en este archivo se encuentra M3,N definitivo    Esta todo unido con "y"
-    for linea in test3:
-        archivo_def3.write(linea)
-        archivo_def3.write('\n')
-    archivo_def3.close()
+                W="[("+PX+", 3, "+P+", "+Turnos[i]+")<>("+PX+", 3, "+P+", "+Turnos[i+1]+")]"
+                M3N.append(W)
+        i+=1
 
+    cont=0
+    prop=""
+    M3Ndef=[]
+    for pp in M3N:
+        if cont==0:
+            prop+="["+pp+"Y"
+            cont+=1
+        elif cont<2:
+            prop+=pp+"Y"
+            cont+=1
 
-    #CREADOR DE LA REGLA5
+        elif cont==2:
+            cont=0
+            prop+=pp+"]"
+            M3Ndef.append(prop)
+            prop=""
 
+    #Listo M3N
+    #==============================================================================================
 
-    myfile = open("cuarto1def.txt","r")
-    x = myfile.readlines()
-    myfile2 = open("cuarto2def.txt","r")
-    y = myfile2.readlines()
-    myfile3=open("cuarto3def.txt","r")
-    z=myfile3.readlines()
-    archivo = open("Regla_4.txt", "w")
-    archivo.seek(0)
+    #UNIR LAS REGLAS
 
-    datos1=[]
-    datos2=[]
-    datos3=[]
-    test_regla4=[]
-    h=""
-    for linea in x:
-        datos1.append(linea)
-    for linea2 in y:
-        datos2.append(linea2)
-    for linea3 in z:
-        datos3.append(linea3)
-    ind=0
-    archivo.write("[")
-    while ind<7:
-        if ind<6:
-            h="[-"+datos1[ind]+">"+"("+datos2[ind]+"Y"+datos3[ind]+")Y-"+datos2[ind]+">"+"("+datos1[ind]+"Y"+datos3[ind]+")Y-"+datos3[ind]+"("+datos1[ind]+"Y"+datos2[ind]+")]Y"+'\n'+'\n'
-            test_regla4.append(h)
-            ind+=1
+    regla=[]
+    r=0
+
+    while r<7: #Todas las listas tienen esta longitud
+        prop="[-"+M1Ndef[r]+">"+"("+M2Ndef[r]+"Y"+M3Ndef[r]+")Y-"+M2Ndef[r]+">"+"("+M1Ndef[r]+"Y"+M3Ndef[r]+")Y-"+M3Ndef[r]+">("+M1Ndef[r]+"Y"+M2Ndef[r]+")]"
+        regla.append(prop)
+        prop=""
+        r+=1
+
+    archivo=open("Regla_4.txt","w")
+
+    Regla4="" #String con toda la regla sin codificar
+
+    r=0
+
+    while r<7:
+        if r==6:
+            Regla4+=regla[r]+")"
+            archivo.write(regla[r]+")")
+        elif r==0:
+            Regla4+="("+regla[r]+"Y"
+            archivo.write("("+regla[r]+"Y"+'\n')
         else:
-            h="[-"+datos1[ind]+">"+"("+datos2[ind]+"Y"+datos3[ind]+")Y-"+datos2[ind]+">"+"("+datos1[ind]+"Y"+datos3[ind]+")Y-"+datos3[ind]+"("+datos1[ind]+"Y"+datos2[ind]+")]]"+'\n'+'\n'
-            test_regla4.append(h)
-            ind+=1
-
-    for linea in test_regla4:
-        archivo.write(linea)
+            Regla4+=regla[r]+"Y"
+            archivo.write(regla[r]+"Y"+'\n')
+        r+=1
     archivo.close()
-    myfile.close()
-    myfile2.close()
-    arch=open("Regla_4.txt", "r")
-    x=arch.readlines()
-
-    Regla4="" #AQUÍ SE GUARDA LA REGLA SIN CODIFICAR
-    for prop in x:
-        Regla4+=prop
-    arch.close()
-    return C.CODIFICAR("Regla_4.txt")
+    retorno=C.CODIFICAR("Regla_4.txt")
+    return retorno
 
 
-def REGLA5():
-    archivo=open("quinto1.txt","w")
+def REGLA5(): #Solo se mueve el de arriba de la torre
     Posiciones=["Arriba","Medio","Abajo"]
     Palos=["PA","PB","PC"]
     Fichas=["F1","F2","F3"]
     Turnos=["1","2","3","4","5","6","7","8"]
-    i=0
-    #M1,N
-    while i<7:#para los turnos
-        parentesis="["
-        archivo.write(parentesis)
-        for PX in Palos:
-            for P in Posiciones:
-                W="[("+PX+", 1, "+P+", "+Turnos[i]+")<>("+PX+", 1, "+P+", "+Turnos[i+1]+")]Y"
-                archivo.write(W)
-        archivo.write("\n")
-        i+=1
-    archivo.close()
-    amix=open("quinto1.txt","r")
-    test=[]
-    lista_de_quinto1=[]
-    lineas_de_quinto1=amix.readlines()
-    for linea in lineas_de_quinto1:
-        lista_de_quinto1.append(linea)
-    for linea in lista_de_quinto1:
-        nueva_linea=linea[0:-2]
-        nueva_linea+="]"
-        test.append(nueva_linea)
-    amix.close()
 
-    archivo_def=open("quinto1def.txt","w")#en este archivo se encuentra M1,N definitivo    Esta todo unido con "y"
-    for linea in test:
-        archivo_def.write(linea)
-        archivo_def.write('\n')
-    archivo_def.close()
+    #M1,N no se necesita en esta regla
 
 
+    #==================================================================================================================
+    #M2N
 
-    archivo2=open("quinto2.txt","w")
+    #Para este hay otras Posiciones
     Posiciones2=["Medio","Abajo"]
-    #M2,n
-    I=0
-    while I<7:
-        parentesis="["
-        archivo2.write(parentesis)
+
+    M2N=[]
+
+    i=0
+    while i<7:
         for PX in Palos:
             for P in Posiciones2:
-                W="[("+PX+", 2, "+P+", "+Turnos[I]+")<>("+PX+", 2, "+P+", "+Turnos[I+1]+")]Y"
-                archivo2.write(W)
-        archivo2.write("\n")
-        I+=1
-    archivo2.close()
-    amix2=open("quinto2.txt","r")
-    test2=[]
-    lista_de_quinto2=[]
-    lineas_de_quinto2=amix2.readlines()
-    for linea in lineas_de_quinto2:
-        lista_de_quinto2.append(linea)
-    for linea in lista_de_quinto2:
-        nueva_linea=linea[0:-2]
-        nueva_linea+="]"
-        test2.append(nueva_linea)
-    amix2.close()
-    archivo_def2=open("quinto2def.txt","w")#en este archivo se encuentra M2,N definitivo    Esta todo unido con "y"
-    for linea in test2:
-        archivo_def2.write(linea)
-        archivo_def2.write('\n')
-    archivo_def2.close()
+                W="[("+PX+", 2, "+P+", "+Turnos[i]+")<>("+PX+", 2, "+P+", "+Turnos[i+1]+")]"
+                M2N.append(W)
+        i+=1
 
-    archivo3=open("quinto3.txt","w")
+    cont=0
+    prop=""
+    M2Ndef=[]
+    for pp in M2N:
+        if cont==0:
+            prop+="["+pp+"Y"
+            cont+=1
+        elif cont<5:
+            prop+=pp+"Y"
+            cont+=1
+
+        elif cont==5:
+            cont=0
+            prop+=pp+"]"
+            M2Ndef.append(prop)
+            prop=""
+
+    """for p in M2Ndef:
+        print(p)"""
+
+    #Listo M2N
+    #==================================================================================================
+    #M3N
+
+    #Para este hay otras Posiciones
     Posiciones3=["Abajo"]
-    #M3,n
-    I=0
-    while I<7:
-        parentesis="["
-        archivo3.write(parentesis)
+    M3N=[]
+
+    i=0
+    while i<7:
         for PX in Palos:
             for P in Posiciones3:
-                W="[("+PX+", 3, "+P+", "+Turnos[I]+")<>("+PX+", 3, "+P+", "+Turnos[I+1]+")]Y"
-                archivo3.write(W)
-        archivo3.write("\n")
-        I+=1
-    archivo3.close()
-    amix3=open("quinto3.txt","r")
-    test3=[]
-    lista_de_quinto3=[]
-    lineas_de_quinto3=amix3.readlines()
-    for linea in lineas_de_quinto3:
-        lista_de_quinto3.append(linea)
-    for linea in lista_de_quinto3:
-        nueva_linea=linea[0:-2]
-        nueva_linea+="]"
-        test3.append(nueva_linea)
-    amix3.close()
-    archivo_def3=open("quinto3def.txt","w")#en este archivo se encuentra M3,N definitivo    Esta todo unido con "y"
-    for linea in test3:
-        archivo_def3.write(linea)
-        archivo_def3.write('\n')
-    archivo_def3.close()
+                W="[("+PX+", 3, "+P+", "+Turnos[i]+")<>("+PX+", 3, "+P+", "+Turnos[i+1]+")]"
+                M3N.append(W)
+        i+=1
 
+    cont=0
+    prop=""
+    M3Ndef=[]
+    for pp in M3N:
+        if cont==0:
+            prop+="["+pp+"Y"
+            cont+=1
+        elif cont<2:
+            prop+=pp+"Y"
+            cont+=1
 
-    #CREADOR DE LA REGLA5
-    myfile = open("quinto1def.txt","r")
-    x = myfile.readlines()
-    myfile2 = open("quinto2def.txt","r")
-    y = myfile2.readlines()
-    myfile3=open("quinto3def.txt","r")
-    z=myfile3.readlines()
-    archivo = open("Regla_5.txt", "w")
-    archivo.seek(0)
+        elif cont==2:
+            cont=0
+            prop+=pp+"]"
+            M3Ndef.append(prop)
+            prop=""
 
-    datos1=[]
-    datos2=[]
-    datos3=[]
-    test_regla5=[]
+    #Listo M3N
+    #==============================================================================================
 
-    h=""
-    for linea in x:
-        datos1.append(linea)
-    for linea2 in y:
-        datos2.append(linea2)
-    for linea3 in z:
-        datos3.append(linea3)
-    archivo.write("[")
-    nuevos_turnos=["1","2","3","4","5","6","7"]
-    ind=0
-    while ind<7:
-        for turno in nuevos_turnos:
-            for Px in Palos:
-                if ind!=6 and Px!="PC" and turno!="7":
-                    h="[[[("+Px+", 2, Abajo, "+turno+")Y("+Px+", 1, Medio, "+turno+")]>"+datos2[ind]+"]Y[[("+Px+", 3, Abajo, "+turno+")Y("+Px+", 2, Medio,"+turno+")Y("+Px+", 1, Arriba, "+turno+")]>["+datos2[ind]+"Y"+datos3[ind]+"]]]Y"
-                    test_regla5.append(h)
-                else:
-                    h="[[[("+Px+", 2, Abajo, "+turno+")Y("+Px+", 1, Medio, "+turno+")]>"+datos2[ind]+"]Y[[("+Px+", 3, Abajo, "+turno+")Y("+Px+", 2, Medio,"+turno+")Y("+Px+", 1, Arriba, "+turno+")]>["+datos2[ind]+"Y"+datos3[ind]+"]]]Y"
-                    test_regla5.append(h)
-            ind+=1
+    #UNIR LAS REGLAS
+    Nturnos=["1","2","3","4","5","6","7"]
 
-    ind2=0
-    while ind<21:
-        if ind!=20:
-            archivo.write(test_regla5[ind2])
-            archivo.write("\n"+"\n")
-            ind+=1
+    regla=[]
+    i=0
+
+    while i<7:
+        for T in Nturnos:
+            for PX in Palos:
+                pp="(((("+PX+", 2, Abajo, "+T+")Y("+PX+", 1, Medio, "+T+"))>"+M2Ndef[i]+")Y((("+PX+", 3, Abajo, "+T+")Y("+PX+", 2, Medio,"+T+")Y("+PX+", 1, Arriba, "+T+"))>("+M2Ndef[i]+"Y"+M3Ndef[i]+")))"
+                regla.append(pp)
+            i+=1
+
+    archivo=open("Regla_5.txt","w")
+
+    Regla5="" #Aquí se guarda toda la regla sin codificar
+
+    r=0
+
+    while r<21:
+        if r==20:
+            Regla5+=regla[r]+")"
+            archivo.write(regla[r]+")")
+        elif r==0:
+            Regla5+="("+regla[r]+"Y"
+            archivo.write("("+regla[r]+"Y"+'\n')
         else:
-            linea=test_regla5[ind]
-            linea_nueva=linea[0:-1]
-            linea_nueva+="]"
-            archivo.write(linea_nueva)
-            archivo.write("\n"+"\n")
-            ind+=1
-
+            Regla5+=regla[r]+"Y"
+            archivo.write(regla[r]+"Y"+'\n')
+        r+=1
 
     archivo.close()
-    myfile.close()
-    myfile2.close()
-    arch=open("Regla_5.txt","r")
-    x=arch.readlines()
+    retorno = C.CODIFICAR("Regla_5.txt")
 
-    Regla5="" #AQUÍ SE GUARDA LA REGLA SIN CODIFICAR
-
-    for prop in x:
-        Regla5+=prop
-
-    arch.close()
-    return C.CODIFICAR("Regla_5.txt")
+    return retorno
 
 
 def CrearReglas():
     return "("+REGLA2()+"Y"+REGLA3()+"Y"+REGLA4()+"Y"+REGLA5()+")"
+
+
+print(CrearReglas())
