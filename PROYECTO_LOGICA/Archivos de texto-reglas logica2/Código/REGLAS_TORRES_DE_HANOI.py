65fe2e6bd5da7cd3120ab610acd186f4d6eae36b
EstefaniaLaverde <57509269+EstefaniaLaverde@users.noreply.github.com>
2020-05-10 18:54:24 -0500

    Update Reglas
    issue no: UD067

--- PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/REGLAS_TORRES_DE_HANOI.py
+++ PROYECTO_LOGICA/Archivos de texto-reglas logica2/Código/REGLAS_TORRES_DE_HANOI.py
@@ -2,6 +2,8 @@
 """ARCHIVO DONDE GUARDO LA FUNCION PARA CREAR CADA REGLAS"""
 
 import Codificacion as C
+import FNCj as F
+import DPLL as D
 
 #FALTA LA REGLA 1
 def REGLA1(): #Una situación por turno
@@ -18,7 +20,7 @@ def REGLA2(): #No levitación
     vacio_abajo=[]
     for T in Turnos:
         for PX in Palos:
-            W="("+"-("+PX+","+"1, Abajo, "+T+" )Y-("+PX+","+"2, Abajo, "+T+")Y-("+PX+","+"3, Abajo, "+T+"))"
+            W="(("+"-("+PX+","+"1, Abajo, "+T+" )Y-("+PX+","+"2, Abajo, "+T+"))Y-("+PX+","+"3, Abajo, "+T+"))"
             vacio_abajo.append(W)
 
     vacio_medio=[]
@@ -32,18 +34,18 @@ def REGLA2(): #No levitación
     vacio_arriba=[]
     for T in Turnos:
         for PX in Palos:
-            W="("+"-("+PX+","+"1, Medio, "+T+" ))"
+            W="(-("+PX+","+"1, Medio, "+T+" ))"
             vacio_arriba.append(W)
 
     it=0
     Regla_2_lista=[]
     while it<len(vacio_abajo): #todos tienen la misma longitud, así que no hay ningún problema
         if it==0:
-            prop="["+"("+vacio_abajo[it]+">"+"("+vacio_medio[it]+"Y"+vacio_arriba[it]+"))Y"
+            prop=""+"("+vacio_abajo[it]+">"+"("+vacio_medio[it]+"Y"+vacio_arriba[it]+"))Y" #OJO
             Regla_2_lista.append(prop)
 
         elif it==len(vacio_abajo)-1:
-            prop="("+vacio_abajo[it]+">"+"("+vacio_medio[it]+"Y"+vacio_arriba[it]+"))"+"]"
+            prop="("+vacio_abajo[it]+">"+"("+vacio_medio[it]+"Y"+vacio_arriba[it]+"))"+""#OJO
             Regla_2_lista.append(prop)
 
         else:
@@ -74,14 +76,14 @@ def REGLA3(): #No mayor sobre menor
     lista_aux=[]
     for T in Turnos:
         for PX in Palos:
-            W="("+PX+",1,Abajo,"+T+")<>-("+PX+",2,Medio,"+T+")Y"
+            W="(("+PX+",1,Abajo,"+T+")<>-("+PX+",2,Medio,"+T+"))Y"
             lista_aux.append(W)
 
     Regla3="("
     i=0
     for i in range(len(lista_aux)):
         if i==len(lista_aux)-1:
-            new_line="("+"PC"+",1,Abajo,"+"8"+")<>-("+"PC"+",2,Medio,"+"8"+")" #Regla sin Y final
+            new_line="(("+"PC"+",1,Abajo,"+"8"+")<>-("+"PC"+",2,Medio,"+"8"+"))" #Regla sin Y final
             Regla3+=new_line
         else:
             Regla3+=lista_aux[i]
@@ -106,28 +108,19 @@ def REGLA4(): #Un movimiento por turno
     while i<7:
         for PX in Palos:
             for P in Posiciones:
-                W="[("+PX+", 1, "+P+", "+Turnos[i]+")<>("+PX+", 1, "+P+", "+Turnos[i+1]+")]"
+                W="(("+PX+", 1, "+P+", "+Turnos[i]+")<>("+PX+", 1, "+P+", "+Turnos[i+1]+"))"
                 M1N.append(W)
         i+=1
 
     cont=0
     prop=""
     M1Ndef=[]
-    for pp in M1N:
-        if cont==0:
-            prop+="["+pp+"Y"
-            cont+=1
-        elif cont<8:
-            prop+=pp+"Y"
-            cont+=1
-        elif cont==8:
-            cont=0
-            prop+=pp+"]"
-            M1Ndef.append(prop)
-            prop=""
-
-    """for p in M1Ndef:
-        print(p)"""
+    #print(len(M1N))=63
+    while cont<63:
+        prop="(((((((("+M1N[cont]+"Y"+ M1N[cont+1]+")Y"+M1N[cont+2]+")Y"+M1N[cont+3]+")Y"+M1N[cont+4]+")Y"+M1N[cont+5]+")Y"+M1N[cont+6]+")Y"+M1N[cont+7]+")Y"+M1N[cont+8]+")"
+        M1Ndef.append(prop)
+        prop=""
+        cont+=9
 
     #LISTO M1N
     #==================================================================================================================
@@ -142,29 +135,20 @@ def REGLA4(): #Un movimiento por turno
     while i<7:
         for PX in Palos:
             for P in Posiciones2:
-                W="[("+PX+", 2, "+P+", "+Turnos[i]+")<>("+PX+", 2, "+P+", "+Turnos[i+1]+")]"
+                W="(("+PX+", 2, "+P+", "+Turnos[i]+")<>("+PX+", 2, "+P+", "+Turnos[i+1]+"))"
                 M2N.append(W)
         i+=1
 
     cont=0
     prop=""
     M2Ndef=[]
-    for pp in M2N:
-        if cont==0:
-            prop+="["+pp+"Y"
-            cont+=1
-        elif cont<5:
-            prop+=pp+"Y"
-            cont+=1
+    while cont<42:
+        prop="((((("+M2N[cont]+"Y"+M2N[cont+1]+")Y"+M2N[cont+2]+")Y"+M2N[cont+3]+")Y"+M2N[cont+4]+")Y"+M2N[cont+5]+")"
+        M2Ndef.append(prop)
+        prop=""
+        cont+=6
 
-        elif cont==5:
-            cont=0
-            prop+=pp+"]"
-            M2Ndef.append(prop)
-            prop=""
 
-    """for p in M2Ndef:
-        print(p)"""
 
     #Listo M2N
     #==================================================================================================
@@ -178,26 +162,19 @@ def REGLA4(): #Un movimiento por turno
     while i<7:
         for PX in Palos:
             for P in Posiciones3:
-                W="[("+PX+", 3, "+P+", "+Turnos[i]+")<>("+PX+", 3, "+P+", "+Turnos[i+1]+")]"
+                W="(("+PX+", 3, "+P+", "+Turnos[i]+")<>("+PX+", 3, "+P+", "+Turnos[i+1]+"))"#OJO
                 M3N.append(W)
         i+=1
 
     cont=0
     prop=""
     M3Ndef=[]
-    for pp in M3N:
-        if cont==0:
-            prop+="["+pp+"Y"
-            cont+=1
-        elif cont<2:
-            prop+=pp+"Y"
-            cont+=1
-
-        elif cont==2:
-            cont=0
-            prop+=pp+"]"
-            M3Ndef.append(prop)
-            prop=""
+    #print(len(M3N))=21
+    while cont<21:
+        prop="(("+M3N[cont]+"Y"+M3N[cont+1]+")Y"+M3N[cont+2]+")"
+        M3Ndef.append(prop)
+        prop=""
+        cont+=3
 
     #Listo M3N
     #==============================================================================================
@@ -208,31 +185,34 @@ def REGLA4(): #Un movimiento por turno
     r=0
 
     while r<7: #Todas las listas tienen esta longitud
-        prop="[-"+M1Ndef[r]+">"+"("+M2Ndef[r]+"Y"+M3Ndef[r]+")Y-"+M2Ndef[r]+">"+"("+M1Ndef[r]+"Y"+M3Ndef[r]+")Y-"+M3Ndef[r]+">("+M1Ndef[r]+"Y"+M2Ndef[r]+")]"
+        prop="(((-"+M1Ndef[r]+">"+"("+M2Ndef[r]+"Y"+M3Ndef[r]+"))Y"
+        prop+="(-"+M2Ndef[r]+">"+"("+M1Ndef[r]+"Y"+M3Ndef[r]+")))Y"
+        prop+="(-"+M3Ndef[r]+">"+"("+M1Ndef[r]+"Y"+M2Ndef[r]+")))"
         regla.append(prop)
         prop=""
         r+=1
 
     archivo=open("Regla_4.txt","w")
 
+
     Regla4="" #String con toda la regla sin codificar
 
     r=0
+    #print("LEN(REGLA)=",len(regla))=7
+
 
     while r<7:
-        if r==6:
-            Regla4+=regla[r]+")"
-            archivo.write(regla[r]+")")
-        elif r==0:
-            Regla4+="("+regla[r]+"Y"
-            archivo.write("("+regla[r]+"Y"+'\n')
-        else:
-            Regla4+=regla[r]+"Y"
-            archivo.write(regla[r]+"Y"+'\n')
-        r+=1
+        Regla4+="(((((("+regla[r]+"Y"+regla[r+1]+")Y"+regla[r+2]+")Y"+regla[r+3]+")Y"+regla[r+4]+")Y"+regla[r+5]+")Y"+regla[r+6]+")"
+        archivo.write(Regla4)
+        r+=7
+
     archivo.close()
     retorno=C.CODIFICAR("Regla_4.txt")
     return retorno
+
+
+
+    """===========================|PRUEBAS|========================"""
 
 
 def REGLA5(): #Solo se mueve el de arriba de la torre
@@ -247,6 +227,8 @@ def REGLA5(): #Solo se mueve el de arriba de la torre
     #==================================================================================================================
     #M2N
 
+    #M2N
+
     #Para este hay otras Posiciones
     Posiciones2=["Medio","Abajo"]
 
@@ -256,29 +238,20 @@ def REGLA5(): #Solo se mueve el de arriba de la torre
     while i<7:
         for PX in Palos:
             for P in Posiciones2:
-                W="[("+PX+", 2, "+P+", "+Turnos[i]+")<>("+PX+", 2, "+P+", "+Turnos[i+1]+")]"
+                W="(("+PX+", 2, "+P+", "+Turnos[i]+")<>("+PX+", 2, "+P+", "+Turnos[i+1]+"))"
                 M2N.append(W)
         i+=1
 
     cont=0
     prop=""
     M2Ndef=[]
-    for pp in M2N:
-        if cont==0:
-            prop+="["+pp+"Y"
-            cont+=1
-        elif cont<5:
-            prop+=pp+"Y"
-            cont+=1
+    while cont<42:
+        prop="((((("+M2N[cont]+"Y"+M2N[cont+1]+")Y"+M2N[cont+2]+")Y"+M2N[cont+3]+")Y"+M2N[cont+4]+")Y"+M2N[cont+5]+")"
+        M2Ndef.append(prop)
+        prop=""
+        cont+=6
 
-        elif cont==5:
-            cont=0
-            prop+=pp+"]"
-            M2Ndef.append(prop)
-            prop=""
 
-    """for p in M2Ndef:
-        print(p)"""
 
     #Listo M2N
     #==================================================================================================
@@ -292,28 +265,20 @@ def REGLA5(): #Solo se mueve el de arriba de la torre
     while i<7:
         for PX in Palos:
             for P in Posiciones3:
-                W="[("+PX+", 3, "+P+", "+Turnos[i]+")<>("+PX+", 3, "+P+", "+Turnos[i+1]+")]"
+                W="(("+PX+", 3, "+P+", "+Turnos[i]+")<>("+PX+", 3, "+P+", "+Turnos[i+1]+"))"#OJO
                 M3N.append(W)
         i+=1
 
     cont=0
     prop=""
     M3Ndef=[]
-    for pp in M3N:
-        if cont==0:
-            prop+="["+pp+"Y"
-            cont+=1
-        elif cont<2:
-            prop+=pp+"Y"
-            cont+=1
+    #print(len(M3N))=21
+    while cont<21:
+        prop="(("+M3N[cont]+"Y"+M3N[cont+1]+")Y"+M3N[cont+2]+")"
+        M3Ndef.append(prop)
+        prop=""
+        cont+=3
 
-        elif cont==2:
-            cont=0
-            prop+=pp+"]"
-            M3Ndef.append(prop)
-            prop=""
-
-    #Listo M3N
     #==============================================================================================
 
     #UNIR LAS REGLAS
@@ -325,7 +290,8 @@ def REGLA5(): #Solo se mueve el de arriba de la torre
     while i<7:
         for T in Nturnos:
             for PX in Palos:
-                pp="(((("+PX+", 2, Abajo, "+T+")Y("+PX+", 1, Medio, "+T+"))>"+M2Ndef[i]+")Y((("+PX+", 3, Abajo, "+T+")Y("+PX+", 2, Medio,"+T+")Y("+PX+", 1, Arriba, "+T+"))>("+M2Ndef[i]+"Y"+M3Ndef[i]+")))"
+                pp="(((("+PX+", 2, Abajo, "+T+")Y("+PX+", 1, Medio, "+T+"))>"+M2Ndef[i]+")Y"
+                pp+="(((("+PX+", 3, Abajo, "+T+")Y("+PX+", 2, Medio,"+T+"))Y("+PX+", 1, Arriba, "+T+"))>("+M2Ndef[i]+"Y"+M3Ndef[i]+")))"
                 regla.append(pp)
             i+=1
 
@@ -354,7 +320,26 @@ def REGLA5(): #Solo se mueve el de arriba de la torre
 
 
 def CrearReglas():
-    return "("+REGLA2()+"Y"+REGLA3()+"Y"+REGLA4()+"Y"+REGLA5()+")"
+    #Archivo para guardar la regla completa por si se quiere ver
+    #archivo=open("Regla_final.txt","w")
 
+    regla_f="("+REGLA2()+"Y"+REGLA3()+"Y"+REGLA4()+"Y"+REGLA5()+")"
+    #archivo.write(regla_f)
 
-print(CrearReglas())
+    #archivo.close()
+    return regla_f
+
+regla=REGLA3()#[:310]
+
+#print("La formula usada fue: \n", regla)
+letras_prop= C.ret_letras_usadas()
+#print("#####################################################")
+ts= F.Tseitin(regla, letras_prop)
+print(len(ts))
+#print("Tseitin: \n",ts)
+#print("=====================================================")
+fClaus = F.formaClausal(ts)
+print("Forma clausal: \n")
+print(fClaus)
+val,I = D.unitPropagate(fClaus,{})
+print(val)
